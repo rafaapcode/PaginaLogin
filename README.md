413a1c0e0474070db55ce9aea66e85c0484341db
rafaapcode <rafaap2003@gmail.com>
2022-02-12 16:46:36 -0300

    pronto

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PaginaLogin
  
+### Sobre o Projeto
+- Criar um página de Login;
+
+### Tecnologias Usadas
+ - HTML5
+ - CSS3
+ - JS
---
+++ js/script.js
@@ -0,0 +1,4 @@
+$('[type="checkbox"]').click(() => {
+    let span = $(".remember").children("span");
+    span.css("color", "rgb(196, 194, 194)");
+});
